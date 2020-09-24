9bbd5439f875783c2dcd6d2709b964172274f602
Sameer Bin Raees <sameerraees@ingenio.ec>
2020-09-24 17:23:59 +0500

    first commit

--- README.md
+++ README.md
@@ -1,68 +1,22 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+![preview](./preview.jpg)
 
-## Available Scripts
+1. Create a list of text-areas, with relative labels, one for each of the following sections:
 
-In the project directory, you can run:
+   - Examination
+   - Clinical History
+   - Technique
+   - Impressions
 
-### `npm start`
+   Add a "Submit" button at the bottom of the page, on click it will `console.log` the text of each of the above fields (`{ examination: 'text', clinicalHistory: 'text', etc... }`);
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+2. Add a little gray outlined circle near each text-area's label:
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+   - When a text-area is focused, make the outline white together with its label;
+   - When the user proceeds to a different text-area, fill it blue with a check symbol in it and reset the label color;
 
-### `npm test`
+3. If the user enters a new line in any of the text-areas, the text-area will grow to make all the lines visible. If the user removes a line, the text-area will shrink with it to fit its content;
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+4. (BONUS POINT) When the "Submit" button is pressed, a little toast notification should appear on the upper-right corner of the page:
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+   - The text should read _"Your report has been successfully submitted"_;
+   - The notification should fade away after 3 seconds;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,301 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { InputLabel } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import classNames from 'classnames';
+import { ToastsContainer, ToastsStore, ToastsContainerPosition } from 'react-toasts';
+
+const CssTextField = withStyles({
+  root: {
+    '& label.Mui-focused': {
+      color: 'white',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: 'white',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'grey',
+      },
+      '&:hover fieldset': {
+        borderColor: '#318bfe',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#318bfe',
+      },
+    },
+  },
+})(TextField);
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      flexGrow: 1,
+    },
+  },
+  field: {
+    width: '100%',
+    color: 'white',
+  },
+  circlew: {
+    height: 20,
+    width: 20,
+    borderStyle: 'solid',
+    borderWidth: 'thin',
+    borderColor: 'white',
+    borderRadius: '50%',
+  },
+  circleg: {
+    height: 20,
+    width: 20,
+    borderStyle: 'solid',
+    borderWidth: 'thin',
+    borderColor: 'grey',
+    borderRadius: '50%',
+  },
+
+  button: {
+    width: '30%',
+    float: 'right',
+  },
+  input: {
+    textAlign: 'left',
+    color: 'grey',
+    marginBottom: 10,
+  },
+  foc: {
+    color: 'grey',
+  },
+  focW: {
+    color: 'white',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
+  const [examination, setExamination] = useState('');
+  const [clinicalHistory, setClinicalHistory] = useState('');
+  const [technique, setTechnique] = useState('');
+  const [impressions, setImpressions] = useState('');
+
+  const [focusExamination, setFocusExamination] = useState(false);
+  const [focusClinicalHistory, setFocusClinicalHistory] = useState(false);
+  const [focusTechnique, setFocusTechnique] = useState(false);
+  const [focusImpressions, setFocusImpressions] = useState(false);
+
+  const displayInfo = () => {
+    console.log('Examination: ', examination);
+    console.log('Clinical History: ', clinicalHistory);
+    console.log('Technique: ', technique);
+    console.log('Impressions: ', impressions);
+    ToastsStore.success('Your report has been successfully submitted');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastsContainer store={ToastsStore} position={ToastsContainerPosition.TOP_RIGHT} />
+      <form className={classes.root} noValidate autoComplete="off">
+        <div>
+          <Grid container spacing={3}>
+            <Grid item>
+              <Box
+                borderRadius="50%"
+                className={`${
+                  examination
+                    ? 't'
+                    : focusExamination || examination
+                    ? classes.circlew
+                    : classes.circleg
+                }`}
+              >
+                &#10003;
+              </Box>
+            </Grid>
+            <Grid item xs={11}>
+              <InputLabel
+                className={classNames(
+                  classes.input,
+                  `${focusExamination ? classes.focW : classes.foc}`,
+                )}
+              >
+                Examination
+              </InputLabel>
+              <CssTextField
+                className={classes.field}
+                classes={{ focused: classes.foc }}
+                InputProps={{
+                  className: classes.field,
+                }}
+                variant="outlined"
+                id="custom-css-outlined-input"
+                value={examination}
+                multiline
+                onChange={(e) => setExamination(e.target.value)}
+                onFocus={() => {
+                  setFocusExamination(true);
+                }}
+                onBlur={() => {
+                  setFocusExamination(false);
+                }}
+              />
+            </Grid>
+          </Grid>
+        </div>
+
+        <div>
+          <Grid container spacing={3}>
+            <Grid item>
+              <Box
+                borderRadius="50%"
+                className={`${
+                  clinicalHistory
+                    ? 't'
+                    : focusClinicalHistory || clinicalHistory
+                    ? classes.circlew
+                    : classes.circleg
+                }`}
+              >
+                &#10003;
+              </Box>
+            </Grid>
+            <Grid item xs={11}>
+              <InputLabel
+                className={classNames(
+                  classes.input,
+                  `${focusClinicalHistory ? classes.focW : classes.foc}`,
+                )}
+              >
+                Clinical History
+              </InputLabel>
+              <CssTextField
+                className={classes.field}
+                classes={{ focused: classes.foc }}
+                InputProps={{
+                  className: classes.field,
+                }}
+                variant="outlined"
+                id="custom-css-outlined-input"
+                value={clinicalHistory}
+                multiline
+                onChange={(e) => setClinicalHistory(e.target.value)}
+                onFocus={() => {
+                  setFocusClinicalHistory(true);
+                }}
+                onBlur={() => {
+                  setFocusClinicalHistory(false);
+                }}
+              />
+            </Grid>
+          </Grid>
+        </div>
+
+        <div>
+          <Grid container spacing={3}>
+            <Grid item>
+              <Box
+                borderRadius="50%"
+                className={`${
+                  technique ? 't' : focusTechnique || technique ? classes.circlew : classes.circleg
+                }`}
+              >
+                &#10003;
+              </Box>
+            </Grid>
+            <Grid item xs={11}>
+              <InputLabel
+                className={classNames(
+                  classes.input,
+                  `${focusTechnique ? classes.focW : classes.foc}`,
+                )}
+              >
+                Technique
+              </InputLabel>
+              <CssTextField
+                className={classes.field}
+                classes={{ focused: classes.foc }}
+                InputProps={{
+                  className: classes.field,
+                }}
+                variant="outlined"
+                id="custom-css-outlined-input"
+                value={technique}
+                multiline
+                onChange={(e) => setTechnique(e.target.value)}
+                onFocus={() => {
+                  setFocusTechnique(true);
+                }}
+                onBlur={() => {
+                  setFocusTechnique(false);
+                }}
+              />
+            </Grid>
+          </Grid>
+        </div>
+
+        <div>
+          <Grid container spacing={3}>
+            <Grid item>
+              <Box
+                borderRadius="50%"
+                className={`${
+                  impressions
+                    ? 't'
+                    : focusImpressions || impressions
+                    ? classes.circlew
+                    : classes.circleg
+                }`}
+              >
+                &#10003;
+              </Box>
+            </Grid>
+            <Grid item xs={11}>
+              <InputLabel
+                className={classNames(
+                  classes.input,
+                  `${focusImpressions ? classes.focW : classes.foc}`,
+                )}
+              >
+                Impressions
+              </InputLabel>
+              <CssTextField
+                className={classes.field}
+                classes={{ focused: classes.foc }}
+                InputProps={{
+                  className: classes.field,
+                }}
+                variant="outlined"
+                id="custom-css-outlined-input"
+                value={impressions}
+                multiline
+                onChange={(e) => setImpressions(e.target.value)}
+                onFocus={() => {
+                  setFocusImpressions(true);
+                }}
+                onBlur={() => {
+                  setFocusImpressions(false);
+                }}
+              />
+            </Grid>
+          </Grid>
+        </div>
+        <br />
+        <div style={{ width: '94%' }}>
+          <Button
+            className={classes.button}
+            variant="contained"
+            style={{ backgroundColor: '#318bfe', color: 'white' }}
+            onClick={displayInfo}
+          >
+            Submit
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
